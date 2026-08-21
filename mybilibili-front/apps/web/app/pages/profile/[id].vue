65ced9b0b89ad5df2aba4c8cfedd2fcca2b7f41c
chentianxiong123 <2036680567@qq.com>
2026-08-21 16:57:54 +0800

    fix: /profile/:id 嵌套路由 Hydration 不匹配
    父页面按是否命中子标签路由条件渲染 <NuxtPage />，避免
    双重渲染 UserProfileView 及 SSR/CSR 节点不一致

--- mybilibili-front/apps/web/app/pages/profile/[id].vue
+++ mybilibili-front/apps/web/app/pages/profile/[id].vue
@@ -1,8 +1,15 @@
 <template>
-  <UserProfileView />
+  <UserProfileView v-if="!isNestedTab" />
+  <NuxtPage v-else />
 </template>
 
 <script setup lang="ts">
 import UserProfileView from '~/views/web/UserProfileView.vue'
 definePageMeta({ layout: 'simple' })
+
+const route = useRoute()
+
+// /profile/:id/xxx 这类子标签路由由 NuxtPage 渲染子页面；
+// 裸访问 /profile/:id 时直接渲染主视图。
+const isNestedTab = computed(() => route.path.split('/').filter(Boolean).length >= 3)
 </script>
