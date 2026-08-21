133c010bc2fdbbed9d0b017b62167baf2e18228a
chentianxiong123 <2036680567@qq.com>
2026-08-21 18:47:08 +0800

    fix: 恢复 /profile/:id 嵌套路由 <NuxtPage /> 分发，修复 E4016 警告

--- mybilibili-front/apps/web/app/pages/profile/[id].vue
+++ mybilibili-front/apps/web/app/pages/profile/[id].vue
@@ -1,8 +1,15 @@
 <template>
-  <UserProfileView />
+  <UserProfileView v-if="!isNestedTab" />
+  <NuxtPage v-else />
 </template>
 
 <script setup lang="ts">
 import UserProfileView from '~/views/web/UserProfileView.vue'
 definePageMeta({ layout: 'simple' })
+
+const route = useRoute()
+
+// /profile/:id 裸路径直接渲染主视图；
+// /profile/:id/xxx 子标签路由交给 NuxtPage 渲染对应子页面。
+const isNestedTab = computed(() => route.path.split('/').filter(Boolean).length >= 3)
 </script>
